2349ae98026117a5aad27ebcd96b1dee3f9a8e87
mrezasaputra <mrezagithub@gmail.com>
2026-04-27 03:48:23 +0700

    Menambahkan Implementasi Runtime Configuration

---
+++ TP_MODUL9_103022400145/CovidConfig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class PengaturanCovid
+{
+    [JsonPropertyName("satuan_suhu")]
+    public string Satuan { get; set; }
+
+    [JsonPropertyName("batas_hari_demam")]
+    public int BatasHari { get; set; }
+
+    [JsonPropertyName("pesan_ditolak")]
+    public string TeksDitolak { get; set; }
+
+    [JsonPropertyName("pesan_diterima")]
+    public string TeksDiterima { get; set; }
+
+    public PengaturanCovid() { }
+
+    public PengaturanCovid(string satuan, int batasHari, string teksDitolak, string teksDiterima)
+    {
+        Satuan = satuan;
+        BatasHari = batasHari;
+        TeksDitolak = teksDitolak;
+        TeksDiterima = teksDiterima;
+    }
+}
+
+public class CovidConfig
+{
+    public PengaturanCovid DataConfig;
+    private const string namaFile = "covid_config.json";
+
+    public CovidConfig()
+    {
+        try
+        {
+            BacaKonfigurasi();
+        }
+        catch (Exception)
+        {
+            AturDefault();
+            TulisKonfigurasiBaru();
+        }
+    }
+
+    private void BacaKonfigurasi()
+    {
+        string teksJson = File.ReadAllText(namaFile);
+        DataConfig = JsonSerializer.Deserialize<PengaturanCovid>(teksJson);
+    }
+
+    private void AturDefault()
+    {
+        DataConfig = new PengaturanCovid(
+            "celcius",
+            14,
+            "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+            "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+        );
+    }
+
+    private void TulisKonfigurasiBaru()
+    {
+        var opsi = new JsonSerializerOptions { WriteIndented = true };
+        string teksJson = JsonSerializer.Serialize(DataConfig, opsi);
+        File.WriteAllText(namaFile, teksJson);
+    }
+
+    public void UbahSatuan()
+    {
+        DataConfig.Satuan = (DataConfig.Satuan == "celcius") ? "fahrenheit" : "celcius";
+        TulisKonfigurasiBaru();
+    }
+}
--- TP_MODUL9_103022400145/Program.cs
+++ TP_MODUL9_103022400145/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig konfigurasi = new CovidConfig();
+
+        Console.WriteLine("--- PENGECEKAN PERTAMA ---");
+        CekStatusMasuk(konfigurasi);
+
+        Console.WriteLine("\n--- MENGUBAH SATUAN SUHU ---");
+        konfigurasi.UbahSatuan();
+        Console.WriteLine($"Satuan suhu berhasil diubah menjadi: {konfigurasi.DataConfig.Satuan}");
+
+        Console.WriteLine("\n--- PENGECEKAN KEDUA ---");
+        CekStatusMasuk(konfigurasi);
+
+        Console.WriteLine("\nSelesai. Tekan enter untuk menutup...");
+        Console.ReadLine();
+    }
+
+    static void CekStatusMasuk(CovidConfig konfigurasi)
+    {
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {konfigurasi.DataConfig.Satuan}");
+        double suhuBadan = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int lamaHari = int.Parse(Console.ReadLine());
+
+        bool isSuhuAman = false;
+
+        if (konfigurasi.DataConfig.Satuan == "celcius")
+        {
+            isSuhuAman = (suhuBadan >= 36.5 && suhuBadan <= 37.5);
+        }
+        else if (konfigurasi.DataConfig.Satuan == "fahrenheit")
+        {
+            isSuhuAman = (suhuBadan >= 97.7 && suhuBadan <= 99.5);
+        }
+
+        bool isHariAman = lamaHari < konfigurasi.DataConfig.BatasHari;
+
+        if (isSuhuAman && isHariAman)
+        {
+            Console.WriteLine(konfigurasi.DataConfig.TeksDiterima);
+        }
+        else
+        {
+            Console.WriteLine(konfigurasi.DataConfig.TeksDitolak);
+        }
+    }
+}
